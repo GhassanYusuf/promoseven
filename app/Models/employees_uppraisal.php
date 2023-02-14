56002a90d6a903c9e1d36a8371309524c2cd2486
GhassanYusuf <Gha366an1983>
2023-02-14 12:49:03 +0300

    fixed uppraisals

--- app/Models/employees_uppraisal.php
+++ app/Models/employees_uppraisal.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class employees_uppraisal extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'eid',
+        'did',
+        'vid',
+        'visa_expire',
+        'position',
+        'duties',
+        'salary',
+        'allowances',
+        'benefits',
+        'documents',
+        'start_date',
+        'end_date',
+        'doneBy',
+    ];
+
 }
